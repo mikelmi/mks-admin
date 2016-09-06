22588c74af16ac3e61abaeab1eaa81af3bbfa5b5
mike_lmi <lmi.inbox@gmail.com>
2016-09-06 20:01:20 +0300

    add lang attribute based on application locale

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" ng-app="admin">
+<html lang="{{ app()->getLocale() or 'en' }}" ng-app="admin">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
