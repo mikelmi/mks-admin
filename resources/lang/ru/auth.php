4ecdc5e13f11a56e91fcabecb78efc97d5639f74
mike_lmi <lmi.inbox@gmail.com>
2016-10-05 11:15:53 +0300

    Add Russian translation

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'title' => 'AdminPanel',
+    'Username' => 'Логин',
+    'Password' => 'Пароль',
+    'Remember me' => 'Запомнить меня',
+    'Sign In' => 'Войти',
+    'Logout' => 'Выйти',
+    'Forgot Password' => 'Забыли пароль?',
+    'failed' => 'Неверные данные для авторизации.',
+    'throttle' => 'Много попыток авторизации. Пожалуйста, попробуйте еще раз через :seconds секунд.',
+    'Reset Password' => 'Восстановить пароль',
+    'send_reset' => 'Отправить',
+    'Access Denied' => 'Доступ запрещен',
+];
