ec7eef19e764786c3573eae4aa0ef3bcd7d5b977
mike_lmi <lmi.inbox@gmail.com>
2017-03-04 21:45:09 +0200

    DataGrid: Add ColumnDate

---
+++ src/DataGrid/Columns/ColumnDate.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: mike
+ * Date: 04.03.17
+ * Time: 21:43
+ */
+
+namespace Mikelmi\MksAdmin\DataGrid\Columns;
+
+
+class ColumnDate extends Column
+{
+    protected $searchType = 'date';
+}
